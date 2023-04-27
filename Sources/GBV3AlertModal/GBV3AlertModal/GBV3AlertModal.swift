fd2a2664954ef88938ef13837193ec1438ded9f5
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-28 05:09:43 +0700

    update default value

--- Sources/GBV3AlertModal/GBV3AlertModal/GBV3AlertModal.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/GBV3AlertModal.swift
@@ -1,12 +1,1 @@
-public var globalProperties = DialogProperties(
-        overlayColor: nil,
-        contentBackgroundColor: nil,
-        contentCornerRadius: nil,
-        contentVerticalMargin: nil,
-        contentHorizontalMargin: nil,
-        contentTopPadding: nil,
-        contentBottomPadding: nil,
-        contentHorizontalPadding: nil,
-        titleFont: nil,
-        titleColor: nil
-)
+public var globalProperties = DialogProperties()
