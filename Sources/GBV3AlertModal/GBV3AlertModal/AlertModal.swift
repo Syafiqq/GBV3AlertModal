7ebf88d7a2743701e26856616afd683e3d242ef5
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-27 16:19:33 +0700

    add alert modal

---
+++ Sources/GBV3AlertModal/GBV3AlertModal/AlertModal.swift
@@ -0,0 +1,90 @@
+import Foundation
+import UIKit
+
+// MARK: - LIFECYCLE AND CALLBACK
+
+public class AlertModal: UIView {
+    // MARK: Outlets
+
+    // MARK: ViewModel
+
+    // MARK: Private Properties
+
+    // Rx
+
+    // MARK: Data
+
+    // Credential
+
+    // MARK: Public Properties
+
+    // MARK: Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        initDesign()
+        initViews()
+        initEvents()
+        initData()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Lifecycle
+
+    // MARK: Override Function
+
+    // MARK: Callback
+
+    // MARK: Public Function
+
+    // MARK: Deinitialization
+
+    deinit {
+    }
+}
+
+// MARK: - PRIVATE FUNCTIONS
+
+private extension AlertModal {
+    // MARK: Init Functions
+    func initViews() {
+    }
+
+    func initEvents() {
+    }
+
+    func initData() {
+    }
+
+    // MARK: Views
+
+    // MARK: ViewModel
+
+    // MARK: Model
+}
+
+// MARK: - DELEGATIONS
+
+// MARK: - EXTENSION
+
+// MARK: - STATIC DETACHABLE
+
+// MARK: - TRACKING
+
+// MARK: - DESIGN
+
+private extension AlertModal {
+    func initDesign() {
+        // MARK: View Initialization
+
+        // MARK: View Graph
+
+        // MARK: View Constraints
+
+        // MARK: View Assign
+    }
+}
