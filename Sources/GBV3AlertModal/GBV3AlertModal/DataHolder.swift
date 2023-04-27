d27097ccbf7c22d3a40ffd0fcaa0d2a3c67454bb
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-28 03:41:20 +0700

    add init

--- Sources/GBV3AlertModal/GBV3AlertModal/DataHolder.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/DataHolder.swift
@@ -14,4 +14,20 @@ public struct DataHolder {
     public var showCloseButton: Bool
     public var dismissOnAction: Bool
     public var completion: ((AlertModal, ActionType) -> Void)?
+
+    public init(title: String?, attributedTitle: NSAttributedString?, subtitle: NSAttributedString?, attributedSubtitle: NSAttributedString?, subtitleCustomView: UIView?, primaryAction: String?, primaryActionStyle: ActionStyle?, secondaryAction: String?, secondaryActionStyle: String?, closeOnTapOverlay: Bool, showCloseButton: Bool, dismissOnAction: Bool, completion: ((AlertModal, ActionType) -> ())?) {
+        self.title = title
+        self.attributedTitle = attributedTitle
+        self.subtitle = subtitle
+        self.attributedSubtitle = attributedSubtitle
+        self.subtitleCustomView = subtitleCustomView
+        self.primaryAction = primaryAction
+        self.primaryActionStyle = primaryActionStyle
+        self.secondaryAction = secondaryAction
+        self.secondaryActionStyle = secondaryActionStyle
+        self.closeOnTapOverlay = closeOnTapOverlay
+        self.showCloseButton = showCloseButton
+        self.dismissOnAction = dismissOnAction
+        self.completion = completion
+    }
 }
