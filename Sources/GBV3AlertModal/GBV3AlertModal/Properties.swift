3256045729e15fc3a9a3b7c384d6c96e6f60b9e7
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-27 21:07:47 +0700

    update

--- Sources/GBV3AlertModal/GBV3AlertModal/Properties.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/Properties.swift
@@ -6,6 +6,7 @@ public struct DialogProperties {
     public var contentCornerRadius: CGFloat?
     public var contentVerticalMargin: CGFloat?
     public var contentHorizontalMargin: CGFloat?
-    public var contentVerticalPadding: (CGFloat, CGFloat)?
+    public var contentTopPadding: (CGFloat, CGFloat)?
+    public var contentBottomPadding: (CGFloat, CGFloat)?
     public var contentHorizontalPadding: (CGFloat, CGFloat)?
 }
