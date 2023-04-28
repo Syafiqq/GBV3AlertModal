1dce82222df42261f4c8d4bc2bf3bd9503c310bc
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-28 15:01:43 +0700

    add adobe xd

---
+++ Sources/GBV3AlertModal/GBV3AlertModal/Extensions/CALayer+AdobeXd.swift
@@ -0,0 +1,34 @@
+import Foundation
+import UIKit
+
+extension CALayer {
+    /// https://stackoverflow.com/questions/34269399/how-to-control-shadow-spread-and-blur
+    func applySketchShadow(
+            color: CGColor = UIColor.black.cgColor,
+            alpha: Float = 0.5,
+            x: CGFloat = 0,
+            y: CGFloat = 2,
+            blur: CGFloat = 4,
+            spread: CGFloat = 0
+    ) {
+        shadowColor = color
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread // swiftlint:disable:this variable_name
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+
+    func removeSketchShadow() {
+        shadowColor = nil
+        shadowOpacity = .zero
+        shadowOffset = .zero
+        shadowRadius = .zero
+        shadowPath = nil
+    }
+}
