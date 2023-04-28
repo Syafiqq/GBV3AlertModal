fedc3d3a7b8e98bd789112d8db6d0f66b5157930
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-28 15:21:25 +0700

    add button themed

--- Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+ButtonAction.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+ButtonAction.swift
@@ -13,13 +13,16 @@ public extension GBAlertModal {
     struct CapsuleThemedAction {
         public var backgroundColor: UIColor?
         public var titleColor: UIColor?
+        public var titleFont: UIFont?
 
         public init(
                 backgroundColor: UIColor? = nil,
-                titleColor: UIColor? = nil
+                titleColor: UIColor? = nil,
+                titleFont: UIFont? = nil
         ) {
             self.backgroundColor = backgroundColor
             self.titleColor = titleColor
+            self.titleFont = titleFont
         }
     }
 
@@ -28,17 +31,20 @@ public extension GBAlertModal {
         public var titleColor: UIColor?
         public var borderWidth: CGFloat?
         public var borderColor: CGColor?
+        public var titleFont: UIFont?
 
         public init(
                 backgroundColor: UIColor? = nil,
                 titleColor: UIColor? = nil,
                 borderWidth: CGFloat? = nil,
-                borderColor: CGColor? = nil
+                borderColor: CGColor? = nil,
+                titleFont: UIFont? = nil
         ) {
             self.backgroundColor = backgroundColor
             self.titleColor = titleColor
             self.borderWidth = borderWidth
             self.borderColor = borderColor
+            self.titleFont = titleFont
         }
     }
 }
@@ -75,13 +81,26 @@ internal extension GBAlertModal {
             button.backgroundColor = style.backgroundColor
             button.setTitleColor(style.titleColor, for: .normal)
             button.setImage(nil, for: .normal)
+            button.titleLabel?.font = style.titleFont
         case .capsuleOutline(let style):
             button.layer.borderWidth = style.borderWidth ?? 0
             button.layer.borderColor = style.borderColor
             button.backgroundColor = style.backgroundColor
             button.setTitleColor(style.titleColor, for: .normal)
             button.setImage(nil, for: .normal)
+            button.titleLabel?.font = style.titleFont
         case .spaceTheme:
+            view.backgroundColor = UIColor.Custom.accentSecondaryDark
+            view.setTitleColor(.white, for: .normal)
+            view.setTitle("send_code".localized.lowercased().capitalizingFirstLetter(), for: .normal)
+            view.titleLabel?.font = FontHelper.Campton.bold.font(16.0)
+            view.layer.applySketchShadow(
+                    color: UIColor.Custom.orangeMandarin,
+                    alpha: 1.0,
+                    x: -2.0,
+                    y: 3.0,
+                    blur: 0.0
+            )
             break
         case .spaceThemeOutline:
             break
@@ -96,7 +115,7 @@ internal extension GBAlertModal {
              .capsuleOutline:
             return generateButtonForCapsuleThemedDesign()
         case .spaceTheme:
-            return generateButtonForCapsuleThemedDesign()
+            return generateButtonForSpaceThemedDesign()
         case .spaceThemeOutline:
             return generateButtonForCapsuleThemedDesign()
         }
@@ -106,6 +125,16 @@ internal extension GBAlertModal {
         let view = GBRoundedButton(type: .system)
         view.translatesAutoresizingMaskIntoConstraints = false
         view.rounded = true
+        view.contentEdgeInsets = UIEdgeInsets(top: 6, left: 16, bottom: 6, right: 16)
+        view.titleLabel?.minimumScaleFactor = 0.5
+        view.titleLabel?.adjustsFontSizeToFitWidth = true
+        return view
+    }
+
+    func generateButtonForSpaceThemedDesign() -> UIButton {
+        let view = UIButton(type: .custom)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 8
         view.contentEdgeInsets = UIEdgeInsets(top: 6, left: 16, bottom: 6, right: 16)
         view.titleLabel?.minimumScaleFactor = 0.5
         view.titleLabel?.adjustsFontSizeToFitWidth = true
