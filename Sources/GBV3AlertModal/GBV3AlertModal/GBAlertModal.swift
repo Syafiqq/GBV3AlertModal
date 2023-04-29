6385e6ce63c4ddd99ad35240e677813d52bfc8f9
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-29 21:26:03 +0700

    GB-1: 002: :lipstick: View, Add GBAlertModal

---
+++ Sources/GBV3AlertModal/GBV3AlertModal/GBAlertModal.swift
@@ -0,0 +1,89 @@
+import Foundation
+import UIKit
+
+// MARK: - LIFECYCLE AND CALLBACK
+
+public class GBAlertModal: UIView {
+    // MARK: Outlets
+
+    // MARK: Constraints
+
+    // MARK: Attributes Gestures
+
+    // MARK: ViewModel
+
+    // MARK: Private Properties
+
+    // MARK: Data
+
+    // MARK: Public Properties
+
+    // MARK: Initialization
+    public override init() {
+        super.init()
+
+        initDesign()
+        initViews()
+        initEvents()
+        initData()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Lifecycle
+
+    // MARK: Override Function
+
+    // MARK: Callback
+
+    // MARK: Public Function
+
+    // MARK: Deinitialization
+
+    deinit {
+    }
+}
+
+// MARK: - PRIVATE FUNCTIONS
+
+private extension GBAlertModal {
+    // MARK: Init Functions
+    func initViews() {
+    }
+
+    func initEvents() {
+    }
+
+    func initData() {
+    }
+
+    // MARK: Views
+
+    // MARK: ViewModel
+
+    // MARK: Model
+}
+
+// MARK: - DELEGATIONS
+
+// MARK: - EXTENSION
+
+// MARK: - STATIC DETACHABLE
+
+// MARK: - TRACKING
+
+// MARK: - DESIGN
+
+private extension GBAlertModal {
+    func initDesign() {
+        // MARK: View Initialization
+
+        // MARK: View Graph
+
+        // MARK: View Constraints
+
+        // MARK: View Assign
+    }
+}
