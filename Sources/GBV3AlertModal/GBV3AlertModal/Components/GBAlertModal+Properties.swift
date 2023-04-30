d757f9b76f63ae072f594ba4846d6b589a367f64
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-30 16:11:44 +0700

    GB-1: 002: :lipstick: GBAlert, Add content property

--- Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+Properties.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+Properties.swift
@@ -14,3 +14,42 @@ extension GBAlertModal {
         }
     }
 }
+
+public extension GBAlertModal.Properties {
+    struct ContentProperty {
+        public static var `default`: Self {
+            Self()
+        }
+
+        public let backgroundColor: UIColor?
+        public let cornerRadius: CGFloat
+        public let fixedWidth: CGFloat?
+        public let maxWidth: CGFloat?
+
+        public init(
+                backgroundColor: UIColor? = nil,
+                cornerRadius: CGFloat = .zero,
+                fixedWidth: CGFloat? = nil,
+                maxWidth: CGFloat? = nil
+        ) {
+            self.backgroundColor = backgroundColor
+            self.cornerRadius = cornerRadius
+            self.fixedWidth = fixedWidth
+            self.maxWidth = maxWidth
+        }
+
+        public func copy(
+                backgroundColor: UIColor? = nil,
+                cornerRadius: CGFloat? = nil,
+                fixedWidth: CGFloat? = nil,
+                maxWidth: CGFloat? = nil
+        ) -> Self {
+            Self(
+                    backgroundColor: backgroundColor ?? self.backgroundColor,
+                    cornerRadius: cornerRadius ?? self.cornerRadius,
+                    fixedWidth: fixedWidth ?? self.fixedWidth,
+                    maxWidth: maxWidth ?? self.maxWidth
+            )
+        }
+    }
+}
