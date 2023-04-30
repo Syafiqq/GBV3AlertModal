7b9349c0868c0c9dad4afbb9cff66ab764234767
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-30 18:13:16 +0700

    GB-1: 004: :lipstick: GBAlertHolder, add overlay holder

--- Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+DataHolder.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+DataHolder.swift
@@ -2,8 +2,12 @@ import UIKit
 
 extension GBAlertModal {
     public struct DataHolder {
+        public let closeOnTapOverlay: Bool
+
         public init(
+                closeOnTapOverlay: Bool = false
         ) {
+            self.closeOnTapOverlay = closeOnTapOverlay
         }
     }
 }
