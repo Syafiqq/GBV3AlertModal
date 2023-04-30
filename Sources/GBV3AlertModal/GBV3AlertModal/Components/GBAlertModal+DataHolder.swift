5d2d9705e38b8ca48e421218a435d50a72703fe1
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-30 16:52:08 +0700

    GB-1: 008: :lipstick: GBAlertHolder, add title data holder

--- Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+DataHolder.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+DataHolder.swift
@@ -6,12 +6,19 @@ extension GBAlertModal {
 
         public let banner: UIImage?
 
+        public let title: String?
+        public let titleAttributed: NSAttributedString?
+
         public init(
                 closeOnTapOverlay: Bool = false,
-                banner: UIImage? = nil
+                banner: UIImage? = nil,
+                title: String? = nil,
+                titleAttributed: NSAttributedString? = nil
         ) {
             self.closeOnTapOverlay = closeOnTapOverlay
             self.banner = banner
+            self.title = title
+            self.titleAttributed = titleAttributed
         }
     }
 }
