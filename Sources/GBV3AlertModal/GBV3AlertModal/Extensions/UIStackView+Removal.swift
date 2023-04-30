abb778227b492d96ea2c391d28a9fd268c50dc9d
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-30 16:23:29 +0700

    GB-1: 002: :lipstick: GBAlert, add stack view extension

---
+++ Sources/GBV3AlertModal/GBV3AlertModal/Extensions/UIStackView+Removal.swift
@@ -0,0 +1,22 @@
+//
+// Created by engineering on 27/4/23.
+//
+
+import UIKit
+
+extension UIStackView {
+    // MARK: Manipulate Subview
+    func removeAllArrangedSubviews() {
+        arrangedSubviews.forEach {
+            removeArrangedSubViewProperly($0)
+        }
+    }
+
+    @discardableResult
+    func removeArrangedSubViewProperly(_ view: UIView) -> UIView {
+        removeArrangedSubview(view)
+        NSLayoutConstraint.deactivate(view.constraints)
+        view.removeFromSuperview()
+        return view
+    }
+}
