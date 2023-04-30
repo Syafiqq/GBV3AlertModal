8a33f552ed0af467ce2123a29f683ba53a4e0d7f
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-30 16:35:26 +0700

    GB-1: 007: :lipstick: GBAlertHolder, add banner

--- Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+DataHolder.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+DataHolder.swift
@@ -4,10 +4,14 @@ extension GBAlertModal {
     public struct DataHolder {
         public let closeOnTapOverlay: Bool
 
+        public let banner: UIImage?
+
         public init(
-                closeOnTapOverlay: Bool = false
+                closeOnTapOverlay: Bool = false,
+                banner: UIImage? = nil
         ) {
             self.closeOnTapOverlay = closeOnTapOverlay
+            self.banner = banner
         }
     }
 }
