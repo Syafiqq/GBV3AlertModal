f894b339d57e7d630d095919bd07eafbc92650fb
msyafiqq <muhammad.syafiq@geniebook.com>
2023-04-30 18:39:00 +0700

    GB-1: 018: :lipstick: GBAlertAction, add capsule theme

--- Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+ButtonAction.swift
+++ Sources/GBV3AlertModal/GBV3AlertModal/Components/GBAlertModal+ButtonAction.swift
@@ -4,10 +4,26 @@ import SnapKit
 
 public extension GBAlertModal {
     enum ActionStyle {
+        case capsule(CapsuleTheme)
     }
 }
 
 public extension GBAlertModal.ActionStyle {
+    struct CapsuleTheme {
+        public var backgroundColor: UIColor?
+        public var titleColor: UIColor?
+        public var titleFont: UIFont?
+
+        public init(
+                backgroundColor: UIColor? = nil,
+                titleColor: UIColor? = nil,
+                titleFont: UIFont? = nil
+        ) {
+            self.backgroundColor = backgroundColor
+            self.titleColor = titleColor
+            self.titleFont = titleFont
+        }
+    }
 }
 
 internal extension GBAlertModal {
@@ -15,11 +31,38 @@ internal extension GBAlertModal {
     }
 
     func configureButtonActionConstraint(_ button: UIButton, parent: UIView, style: ActionStyle) {
+        switch style {
+        case .capsule:
+            button.snp.makeConstraints { (make: ConstraintMaker) -> Void in
+                // Align
+                make.edges
+                        .equalToSuperview()
+            }
+
+            parent.snp.makeConstraints { (make: ConstraintMaker) -> Void in
+                // Pin
+                make.height
+                        .equalTo(48)
+            }
+        }
     }
 }
 
 internal extension GBAlertModal {
     func generateButtonForActionDesign(style: ActionStyle) -> UIButton {
-        fatalError("not yet implemented")
+        switch style {
+        case .capsule:
+            return generateButtonForCapsuleThemedDesign()
+        }
+    }
+
+    func generateButtonForCapsuleThemedDesign() -> GBRoundedButton {
+        let view = GBRoundedButton(type: .system)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.rounded = true
+        view.contentEdgeInsets = UIEdgeInsets(top: 6, left: 16, bottom: 6, right: 16)
+        view.titleLabel?.minimumScaleFactor = 0.5
+        view.titleLabel?.adjustsFontSizeToFitWidth = true
+        return view
     }
 }
